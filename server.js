3c485813b4d97cd763fcf5998612444eb41bcd52
Shiva7ganesh <22r01a0531@cmrithyderabad.edu.in>
2025-03-10 15:37:59 +0530

    changes

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Explicit route handler for the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const RESULT_URL = "https://results.cmrithyderabad.edu.in/helper.php?gamaOne=getResult";
 
